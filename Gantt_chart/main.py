7706360ac0baf77876e449d755f2fc603cee6124
SqrtMinusOne <thexcloud@gmail.com>
2018-09-12 22:46:33 +0300

    Added Gantt chart

---
+++ Gantt_chart/main.py
@@ -0,0 +1,108 @@
+from matplotlib import pyplot as plt
+from random import uniform
+
+
+def get_color():
+    return [uniform(0, 255) / 255, uniform(0, 255) / 255, uniform(0, 255) / 255]
+
+
+def plot_p(axes, m_t, n_t, p_t, t_t):
+    pl_t = 0
+    for i in range(m_t):  # Plotting
+        col = get_color()
+        for k in range(0, t_t[i] * n_t, t_t[i]):
+            axes.plot([k + pl_t, k + pl_t + t_t[i]], [i, i], color=col, **line_params)
+        pl_t = pl_t + t_t[i] * n_t
+        if i != m_t - 1:
+            axes.arrow(pl_t, i, 0, 1, **arrow_params)
+    return pl_t
+
+
+def plot_pm(axes, m_t, n_t, p_t, t_t):
+    pl_t = 0
+    major_i, major_t = 0, 0
+    col = [get_color() for i in range(m_t)]
+    for i in range(m_t):  # Getting the major operation
+        if t_t[i] > major_t:
+            major_t = t_t[i]
+            #  major_i = i
+    for k in range(int(n_t / p_t)):  # Plotting
+        pl_t = k * major_t * p_t
+        for i in range(m_t):
+            axes.plot([pl_t, pl_t + t_t[i] * p_t], [i, i], color=col[i])
+            pl_t = pl_t + t_t[i] * p_t
+            if i != m_t - 1:
+                axes.arrow(pl_t, i, 0, 1, **arrow_params)
+    return pl_t
+
+
+def plot_pp(axes, m_t, n_t, p_t, t_t):
+    pl_t = 0
+    for i in range(m_t):
+        col = get_color()
+        draw_arrow = False
+        if i != m_t - 1:
+            if t_t[i] < t_t[i + 1]:
+                draw_arrow = True
+        for k in range(0, t_t[i] * n_t, t_t[i]):
+            axes.plot([k + pl_t, k + pl_t + t_t[i]], [i, i], color=col, **line_params)
+            if draw_arrow:
+                axes.arrow(k + pl_t + t_t[i], i, 0, 1, **arrow_params)
+        if draw_arrow:
+            pl_t = pl_t + t_t[i] * p_t
+        elif i != m_t - 1:
+            pl_t = pl_t + (t_t[i] * n_t) - (n_t - p_t) * t_t[i + 1]
+            axes.arrow(pl_t, i, 0, 1, **arrow_params)
+            axes.arrow(pl_t + (n_t - p_t) * t_t[i + 1], i, 0, 1, **arrow_params)
+        else:
+            pl_t = pl_t + t_t[i] * n_t
+
+    return pl_t
+
+
+def enhance(axes, pl_t):  # Post-plotting enhancement
+    y_min, y_max = axes.get_ylim()
+    axes.set_ylim(y_max + 2, y_min)
+    x_min, x_max = axes.get_xlim()
+    plt.xticks(range(pl_t + 1))
+    plt.annotate('', xy=(0, m), xytext=(pl_t, m), arrowprops=dict(arrowstyle='<->', linewidth=2))
+    plt.text(int(pl_t / 2), m - 0.1, '$T_c$', fontsize=15)
+
+
+def init():  # initialization
+    figure = plt.figure()
+    axes = figure.add_axes([0.08, 0.1, 0.87, 0.8])
+    plt.yticks(range(m))
+    axes.set_xlabel("t,мин", position=(1, 0), size=15)
+    axes.set_ylabel("i", position=(0, 1), rotation='horizontal', size=15)
+    plt.grid(True)
+    return figure, axes
+
+
+def draw(m, n, p, t, mode):
+    fig, main_axes = init()
+    pl = 0
+    if mode == 'p':
+        pl = plot_p(main_axes, m, n, p, t)
+    elif mode == 'pm':
+        pl = plot_pm(main_axes, m, n, p, t)
+    else:
+        pl = plot_pp(main_axes, m, n, p, t)
+    enhance(main_axes, pl)
+
+
+# parameters
+m = 3  # Number of operations
+n = 4  # Size of a batch
+p = 1  # Size of a transfer batch
+t = [2, 3, 1]  # Time of execution of a operation
+
+line_params = dict(marker='|', markersize=10, linewidth=2)  # Line parameters
+arrow_params = dict(width=0.02, head_width=0.2, head_length=0.1, length_includes_head=True, color='black')
+fig_n = 0
+
+draw(m, n, p, t, 'p')
+draw(m, n, p, t, 'pm')
+draw(m, n, p, t, 'pp')
+
+plt.show()
